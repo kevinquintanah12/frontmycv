2f4d4c3b4612992b7ff5880e2d0c2daa0a865acc
adsoftsito <adsoft@live.com.mx>
2024-03-14 16:54:14 -0600

    update routes #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
